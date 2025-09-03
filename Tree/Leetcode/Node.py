13ce4ec403bd3f0bfdb3f1591ae40bb03c4952cc
naveennk045 <naveennk045@gmail.com>
2025-09-03 15:24:44 +0530

    feat: add height of the of binary tree

---
+++ Tree/Leetcode/Node.py
@@ -0,0 +1,5 @@
+class TreeNode:
+    def __init__(self, val : int  =0 , left : "TreeNode" = None, right :"TreeNode" = None):
+        self.val = val
+        self.left = left
+        self.right = right
---
+++ Tree/Leetcode/depth_binary_tree.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from Tree.Leetcode.Node import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        max_height: int = 0
+
+        def max_depth_helper(root: TreeNode, height):
+            nonlocal max_height
+            if root is None:
+                return
+
+            if root.right is None and root.left is None:
+                max_height = max(max_height, height)
+
+            max_depth_helper(root.left, height + 1)
+            max_depth_helper(root.right, height + 1)
+
+        max_depth_helper(root, 0)
+
+        return max_height
